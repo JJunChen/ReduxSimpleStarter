014c6dc89d1c80ce34d54d06db3795d925717e01
Jiajun Chen <jchen0318@gmail.com>
2018-11-10 07:49:21 -0800

    connect searchBar with api search

--- src/components/searth_bar.js
+++ src/components/searth_bar.js
@@ -8,16 +8,16 @@ class SearchBar extends Component {
 
     this.onInputChange = this.onInputChange.bind(this);
   }
-  
+
   onInputChange(event) {
     const term = event.target.value;
     this.setState({term});
-    console.log(this.state.term); 
+    this.props.onSearchTermChange(term); 
   }
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input 
           value={this.state.term}
           onChange={this.onInputChange}/>
